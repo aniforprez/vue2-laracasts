735f8aba0e0be542b89624a668d17e42a7d80a11
Anirudh S <ani.22691@gmail.com>
2017-02-10 09:41:32 +0530

    Finished 'Components 101'

---
+++ main.js
@@ -0,0 +1,42 @@
+Vue.component('task', {
+    template: '<li><slot></slot></li>'
+});
+
+let app = new Vue({
+    el: '#root',
+    data: {
+        title: 'This is some bullshit',
+        newName: '',
+        message: 'Hello World',
+        names: ['Ani', 'Avi', 'Amma', 'Accha'],
+        className: 'colorRed',
+        isLoading: false,
+        isDisabled: false,
+        tasks: [
+            { description: 'Do something', completed: false },
+            { description: 'Do nothing', completed: true },
+            { description: 'Do anything', completed: true },
+            { description: 'Whatever', completed: false }
+        ]
+    },
+    methods: {
+        addName() {
+            this.names.push(this.newName);
+            this.newName = '';
+        },
+        toggleClass() {
+            this.isLoading = !this.isLoading;
+        },
+        toggleDisabled() {
+            this.isDisabled = !this.isDisabled;
+        }
+    },
+    computed: {
+        reversedMessage() {
+            return this.message.split('').reverse().join('');
+        },
+        incompleteTasks() {
+            return this.tasks.filter((task) => !task.completed);
+        }
+    }
+});
