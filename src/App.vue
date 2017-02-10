af4473ee1193ec08af8702f77a1f72786ff438c0
Anirudh S <ani.22691@gmail.com>
2017-02-10 18:17:29 +0530

    Finished ' Webpack and vue-cli '

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+	<div id="app">
+		<message>Yo what up</message>
+	</div>
+</template>
+
+<script>
+import Message from './components/Message.vue';
+export default {
+	name: 'app',
+	data () {
+		return {
+
+		}
+	},
+	components: [Message]
+}
+</script>
+
+<style>
+
+</style>
